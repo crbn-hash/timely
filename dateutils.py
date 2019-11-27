e74dd504dd9f21121bba4164a4b79caa696bfcbd
crbn-hash <jeremy@crbn.dev>
2019-11-27 13:28:07 -0500

    dateutils methods

---
+++ dateutils.py
@@ -0,0 +1,48 @@
+#returns a list of date objects for all days of the current month
+import datetime
+
+def get_month_dates():
+    dates_before = []
+    dates_after = []
+    dates_in_month = []
+    today = datetime.date.today()
+    daysbefore = today.day - 1
+    while (daysbefore >= 1):
+        tdelta = datetime.timedelta(days=daysbefore)
+        dates_before.append(today - tdelta)
+        daysbefore -= 1
+    
+    daysafter = 0
+    while(today.month == (today+datetime.timedelta(days=daysafter)).month):
+        tdelta = datetime.timedelta(days=daysafter)
+        dates_after.append(today + tdelta)
+        daysafter += 1
+    dates_before.extend(dates_after)
+    dates_in_month.extend(dates_before)
+    #clean up this mongrel code
+    return dates_in_month
+
+def get_workweek():
+
+    days_in_week = 7
+    weekdays = []
+    today = datetime.date.today()
+    tdelta = datetime.timedelta(days=today.weekday())
+    monday = today - tdelta
+
+    for day in range(days_in_week):
+        weekdays.append(monday + datetime.timedelta(days=day))
+    return weekdays
+
+def number_to_name(date):
+    #confirm that param is a valid date
+    switch = {
+        0: 'Monday',
+        1: 'Tuesday',
+        2: 'Wednesday',
+        3: 'Thursday',
+        4: 'Friday',
+        5: 'Saturday',
+        6: 'Sunday'
+        }
+    return switch.get(date.weekday(), 'Error generating day name from given number')
