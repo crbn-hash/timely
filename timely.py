b85de3d32b992ea6449c74ac770c8b8c8e42acd3
crbn-hash <jeremy@crbn.dev>
2019-12-01 22:09:01 -0500

    add passing sched object in template, format via javascript not jinja2

--- timely.py
+++ timely.py
@@ -27,7 +27,7 @@ class Employee(db.Model):
         self.username = username
 
     def __repr__(self):
-        return "<Employee '{}'>".format(self.username)
+        return ">{}".format(self.username)
 
 class Schedule(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -71,7 +71,7 @@ class ScheduleView(ModelView):
     edit_modal=True
 class ShiftView(ModelView):
     column_list = ['title','start_dt', 'end_dt', 'schedule', 'employees']
-    column_editable_list=['title', 'start_dt', 'end_dt']
+    column_editable_list=['title', 'start_dt', 'end_dt', 'employees']
     edit_modal=True
 class EmployeeView(ModelView):
     column_list = ['username', 'shifts', 'email']
@@ -87,12 +87,27 @@ admin.add_view(EmployeeView(Employee, db.session))
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/schedule')
 def schedule2():
     WEEKS_IN_MONTH = 4
     month = dateutils.get_workweeks(WEEKS_IN_MONTH)
     shifts = Shift.query.all()
     return (render_template('schedule.html',shifts=shifts,month=month, weekdays=month[0]))
+
+#which schedule
+#change back to filter - is this a jinja error because of query object?
+#a gill for forgetting that the URL param must be passed to the route function
+#as a parameter, otherwise it cannot be used inside the function
+#expand to return full schedule object. instead of generating schedule by fitting 
+#it into html via Jinja2, send it as JSON and let javascript do the formatting(alternative)
+@app.route('/schedule/<title>')
+def schedule(title):
+    sched = Schedule.query.filter_by(title=title).first()
+    return (render_template('schedule2.html', schedule=sched))
 
 if __name__ == "__main__":
     app.run(debug=True) 
